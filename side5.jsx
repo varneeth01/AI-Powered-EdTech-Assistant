70af5d36061fa8af1ab9b8b610c49e9b1f7b80b1
PoojaMaulekhi <poojamaulekhi7@gmail.com>
2025-03-09 16:23:46 +0530

    Create side5.jsx

---
+++ side5.jsx
@@ -0,0 +1,34 @@
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import QuizIcon from '@mui/icons-material/Quiz';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+export default function Sfive() {
+  return (
+    <Button
+      component="label"
+      role={undefined}
+      variant="contained"
+      tabIndex={-1}
+      startIcon={<QuizIcon/>}
+    >
+     Test And Quizes
+      <VisuallyHiddenInput
+        type="file"
+        onChange={(event) => console.log(event.target.files)}
+        multiple
+      />
+    </Button>
+  );
+}
